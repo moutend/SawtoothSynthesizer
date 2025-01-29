a522337faaef5748ff03a8ef2cee323b3fa1bda3
Yoshiyuki Koyanagi <moutend@gmail.com>
2025-01-29 18:38:54 +0900

    Create project

---
+++ SawtoothSynthesizer/SawtoothSynthesizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SawtoothSynthesizerApp.swift
+//  SawtoothSynthesizer
+//
+//  Created by Yoshiyuki Koyanagi on 2025/01/29.
+//
+
+import SwiftUI
+
+@main
+struct SawtoothSynthesizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
