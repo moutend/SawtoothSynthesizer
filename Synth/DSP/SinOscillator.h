5d4dfe619832eaa6ea52cd56f6788554f53289d6
Yoshiyuki Koyanagi <moutend@gmail.com>
2025-01-29 21:03:14 +0900

    Implement sawtooth synthesizer

--- Synth/DSP/SinOscillator.h
+++ Synth/DSP/SinOscillator.h
@@ -6,30 +6,88 @@
 //
 
 #pragma once
-
-#include <numbers>
 #include <cmath>
+#include <numbers>
+
+// 0から127の範囲外のノートナンバーを受信できてしまうため制限する。
+constexpr int NumberOfPianoKeys = 127;
+constexpr double Pi2 = 2.0 * std::numbers::pi_v<double>;
 
 class SinOscillator {
 public:
-    SinOscillator(double sampleRate = 44100.0) {
-        mSampleRate = sampleRate;
+  SinOscillator(double sampleRate = 44100.0) {
+    mSampleRate = sampleRate;
+
+    for (int i = 0; i < NumberOfPianoKeys; i++) {
+      // The A4 note value is 69.
+      const double frequency = 440.0 * std::pow(2.0, (i - 69) / 12.0);
+
+      mDeltaOmegas[i] =
+          frequency * 2.0 * std::numbers::pi_v<double> / sampleRate;
+      mOmegas[i] = 0.0;
+      mDeltaGates[i] = 0.0;
+      mGates[i] = 0.0;
+    }
+  }
+  void setFrequency(double frequency) { mDeltaOmega = frequency / mSampleRate; }
+  void noteOn(int noteIndex) {
+    if (noteIndex < 0 || noteIndex > NumberOfPianoKeys - 1) {
+      return;
     }
 
-    void setFrequency(double frequency) {
-        mDeltaOmega = frequency / mSampleRate;
+    const double deltaGate = 100.0 / mSampleRate;
+
+    mDeltaGates[noteIndex] = deltaGate;
+  }
+  void noteOff(int noteIndex) {
+    if (noteIndex < 0 || noteIndex > NumberOfPianoKeys - 1) {
+      return;
     }
 
-    double process() {
-        const double sample = std::sin(mOmega * (std::numbers::pi_v<double> * 2.0));
-        mOmega += mDeltaOmega;
+    const double deltaGate = 100.0 / mSampleRate;
+    mDeltaGates[noteIndex] = -deltaGate;
+  }
+  double process() {
+    double signal = 0.0;
 
-        if (mOmega >= 1.0) { mOmega -= 1.0; }
-        return sample;
+    for (int i = 0; i < NumberOfPianoKeys; i++) {
+      const double sinVolume = double(i) / double(NumberOfPianoKeys);
+      const double sinSignal = mGates[i] * std::sin(mOmegas[i]);
+      const double sawVolume =
+          double(NumberOfPianoKeys - i) / double(NumberOfPianoKeys);
+      const double sawSignal =
+          mGates[i] * (2.0 * (1.0 - mOmegas[i] / Pi2) - 1.0);
+
+      signal += sinVolume * sinSignal;
+      signal += sawVolume * sawSignal;
+
+      mOmegas[i] += mDeltaOmegas[i];
+
+      if (mOmegas[i] >= Pi2) {
+        mOmegas[i] -= Pi2;
+      }
+
+      mGates[i] += mDeltaGates[i];
+
+      if (mGates[i] >= 1.0) {
+        mGates[i] = 1.0;
+        mDeltaGates[i] = 0.0;
+      }
+      if (mGates[i] <= 0.0) {
+        mGates[i] = 0.0;
+        mDeltaGates[i] = 0.0;
+      }
     }
+
+    return signal;
+  }
 
 private:
-    double mOmega = { 0.0 };
-    double mDeltaOmega = { 0.0 };
-    double mSampleRate = { 0.0 };
+  double mOmega = {0.0};
+  double mDeltaOmega = {0.0};
+  double mSampleRate = {0.0};
+  double mDeltaOmegas[NumberOfPianoKeys];
+  double mOmegas[NumberOfPianoKeys];
+  double mGates[NumberOfPianoKeys];
+  double mDeltaGates[NumberOfPianoKeys];
 };
